d79a02af760265bf371202c4f884761e5e069158
Maksym <tigron1994@gmail.com>
2021-09-11 18:01:12 +0200

    Добавляю стартовые файлы проекта

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import ContactForm from 'components/ContactForm';
+import ContactList from 'components/ContactList';
+import Filter from 'components/Filter';
+import { v4 as uuidv4 } from 'uuid';
+import s from 'App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleSubmit = e => {
+    if (this.state.contacts.some(contact => contact.name === e.name)) {
+      alert(`${e.name} is already in contacts.`);
+      return;
+    }
+    const NewContact = {
+      name: e.name,
+      number: e.number,
+      id: uuidv4(),
+    };
+
+    this.setState(prevState => ({
+      contacts: [NewContact, ...prevState.contacts],
+    }));
+  };
+
+  handleDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+  };
+
+  render() {
+    const { name, number, filter, contacts } = this.state;
+    const { handleSubmit, handleChange, handleDeleteContact, getFilteredContacts, changeFilter } =
+      this;
+    return (
+      <div>
+        <h2 className={s.title}>Phonebook</h2>
+        <ContactForm onSubmit={handleSubmit} name={name} number={number} onChange={handleChange} />
+        {contacts.length > 1 && <Filter filter={filter} onChange={changeFilter} />}
+        <h2 className={s.title}>Contacts</h2>
+        <ContactList contacts={getFilteredContacts()} onDeleteContacts={handleDeleteContact} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import s from 'components/ContactForm/ContactForm.module.css';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <form className={s.form} onSubmit={this.handleSubmit}>
+        <label>
+          <h2 className={s.labelTitle}>Name</h2>
+          <input
+            className={s.input}
+            value={this.state.name}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          <h2 className={s.labelTitle}>Number</h2>
+          <input
+            className={s.input}
+            value={this.state.number}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <button className={s.button} type="submit">
+          Add to contacts
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
